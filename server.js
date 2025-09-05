2b9af70d1cd5b3b7cef521d5013bdf622a02b1ca
donski <136574676+Pedroso0417@users.noreply.github.com>
2025-09-05 11:41:25 +0800

    commit codes

--- server.js
+++ server.js
@@ -54,6 +54,7 @@ app.post("/bookings", async (req, res) => {
 });
 
 // Start server
-app.listen(PORT, () => {
-  console.log(`🚀 Server running on http://localhost:${PORT}`);
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`🚀 Server running on http://0.0.0.0:${PORT}`);
 });
+
